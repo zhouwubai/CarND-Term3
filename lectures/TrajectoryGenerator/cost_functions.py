ce30e229534ac66dbd979b55dfcce79b9260270a
Wubai Zhou <wubai.zhou@uber.com>
2018-05-06 11:38:29 -0700

    fix trajectory generator code style

--- lectures/TrajectoryGenerator/cost_functions.py
+++ lectures/TrajectoryGenerator/cost_functions.py
@@ -1,18 +1,25 @@
-from helpers import logistic, to_equation, differentiate, nearest_approach_to_any_vehicle, get_f_and_N_derivatives
+from helpers import (
+    logistic, to_equation, differentiate,
+    nearest_approach_to_any_vehicle,
+    get_f_and_N_derivatives
+)
 from constants import *
 import numpy as np
+
+
 # COST FUNCTIONS
 def time_diff_cost(traj, target_vehicle, delta, T, predictions):
     """
-    Penalizes trajectories that span a duration which is longer or 
+    Penalizes trajectories that span a duration which is longer or
     shorter than the duration requested.
     """
     _, _, t = traj
-    return logistic(float(abs(t-T)) / T)
+    return logistic(float(abs(t - T)) / T)
+
 
 def s_diff_cost(traj, target_vehicle, delta, T, predictions):
     """
-    Penalizes trajectories whose s coordinate (and derivatives) 
+    Penalizes trajectories whose s coordinate (and derivatives)
     differ from the goal.
     """
     s, _, T = traj
@@ -22,17 +29,18 @@ def s_diff_cost(traj, target_vehicle, delta, T, predictions):
     S = [f(T) for f in get_f_and_N_derivatives(s, 2)]
     cost = 0
     for actual, expected, sigma in zip(S, s_targ, SIGMA_S):
-        diff = float(abs(actual-expected))
-        cost += logistic(diff/sigma)
+        diff = float(abs(actual - expected))
+        cost += logistic(diff / sigma)
     return cost
+
 
 def d_diff_cost(traj, target_vehicle, delta, T, predictions):
     """
-    Penalizes trajectories whose d coordinate (and derivatives) 
+    Penalizes trajectories whose d coordinate (and derivatives)
     differ from the goal.
     """
     _, d_coeffs, T = traj
-    
+
     d_dot_coeffs = differentiate(d_coeffs)
     d_ddot_coeffs = differentiate(d_dot_coeffs)
 
@@ -41,36 +49,43 @@ def d_diff_cost(traj, target_vehicle, delta, T, predictions):
     d_ddot = to_equation(d_ddot_coeffs)
 
     D = [d(T), d_dot(T), d_ddot(T)]
-    
+
     target = predictions[target_vehicle].state_in(T)
     target = list(np.array(target) + np.array(delta))
     d_targ = target[3:]
     cost = 0
     for actual, expected, sigma in zip(D, d_targ, SIGMA_D):
-        diff = float(abs(actual-expected))
-        cost += logistic(diff/sigma)
+        diff = float(abs(actual - expected))
+        cost += logistic(diff / sigma)
     return cost
+
 
 def collision_cost(traj, target_vehicle, delta, T, predictions):
     """
     Binary cost function which penalizes collisions.
     """
     nearest = nearest_approach_to_any_vehicle(traj, predictions)
-    if nearest < 2*VEHICLE_RADIUS: return 1.0
-    else : return 0.0
+    if nearest < 2 * VEHICLE_RADIUS:
+        return 1.0
+    else:
+        return 0.0
+
 
 def buffer_cost(traj, target_vehicle, delta, T, predictions):
     """
     Penalizes getting close to other vehicles.
     """
     nearest = nearest_approach_to_any_vehicle(traj, predictions)
-    return logistic(2*VEHICLE_RADIUS / nearest)
-    
+    return logistic(2 * VEHICLE_RADIUS / nearest)
+
+
 def stays_on_road_cost(traj, target_vehicle, delta, T, predictions):
     pass
 
+
 def exceeds_speed_limit_cost(traj, target_vehicle, delta, T, predictions):
     pass
+
 
 def efficiency_cost(traj, target_vehicle, delta, T, predictions):
     """
@@ -81,7 +96,8 @@ def efficiency_cost(traj, target_vehicle, delta, T, predictions):
     avg_v = float(s(t)) / t
     targ_s, _, _, _, _, _ = predictions[target_vehicle].state_in(t)
     targ_v = float(targ_s) / t
-    return logistic(2*float(targ_v - avg_v) / avg_v)
+    return logistic(2 * float(targ_v - avg_v) / avg_v)
+
 
 def total_accel_cost(traj, target_vehicle, delta, T, predictions):
     s, d, t = traj
@@ -93,21 +109,24 @@ def total_accel_cost(traj, target_vehicle, delta, T, predictions):
     for i in range(100):
         t = dt * i
         acc = a(t)
-        total_acc += abs(acc*dt)
+        total_acc += abs(acc * dt)
     acc_per_second = total_acc / T
-    
-    return logistic(acc_per_second / EXPECTED_ACC_IN_ONE_SEC )
-    
+
+    return logistic(acc_per_second / EXPECTED_ACC_IN_ONE_SEC)
+
+
 def max_accel_cost(traj, target_vehicle, delta, T, predictions):
     s, d, t = traj
     s_dot = differentiate(s)
     s_d_dot = differentiate(s_dot)
     a = to_equation(s_d_dot)
-    all_accs = [a(float(T)/100 * i) for i in range(100)]
+    all_accs = [a(float(T) / 100 * i) for i in range(100)]
     max_acc = max(all_accs, key=abs)
-    if abs(max_acc) > MAX_ACCEL: return 1
-    else: return 0
-    
+    if abs(max_acc) > MAX_ACCEL:
+        return 1
+    else:
+        return 0
+
 
 def max_jerk_cost(traj, target_vehicle, delta, T, predictions):
     s, d, t = traj
@@ -115,10 +134,13 @@ def max_jerk_cost(traj, target_vehicle, delta, T, predictions):
     s_d_dot = differentiate(s_dot)
     jerk = differentiate(s_d_dot)
     jerk = to_equation(jerk)
-    all_jerks = [jerk(float(T)/100 * i) for i in range(100)]
+    all_jerks = [jerk(float(T) / 100 * i) for i in range(100)]
     max_jerk = max(all_jerks, key=abs)
-    if abs(max_jerk) > MAX_JERK: return 1
-    else: return 0
+    if abs(max_jerk) > MAX_JERK:
+        return 1
+    else:
+        return 0
+
 
 def total_jerk_cost(traj, target_vehicle, delta, T, predictions):
     s, d, t = traj
@@ -130,6 +152,6 @@ def total_jerk_cost(traj, target_vehicle, delta, T, predictions):
     for i in range(100):
         t = dt * i
         j = jerk(t)
-        total_jerk += abs(j*dt)
+        total_jerk += abs(j * dt)
     jerk_per_second = total_jerk / T
-    return logistic(jerk_per_second / EXPECTED_JERK_IN_ONE_SEC )
+    return logistic(jerk_per_second / EXPECTED_JERK_IN_ONE_SEC)
--- lectures/TrajectoryGenerator/helpers.py
+++ lectures/TrajectoryGenerator/helpers.py
@@ -1,13 +1,14 @@
 from math import sqrt, exp
 from matplotlib import pyplot as plt
 
+
 class Vehicle(object):
     """
     Helper class. Non-ego vehicles move w/ constant acceleration
     """
     def __init__(self, start):
         self.start_state = start
-    
+
     def state_in(self, t):
         s = self.start_state[:3]
         d = self.start_state[3:]
@@ -21,14 +22,16 @@ class Vehicle(object):
         ]
         return state
 
+
 def logistic(x):
     """
-    A function that returns a value between 0 and 1 for x in the 
+    A function that returns a value between 0 and 1 for x in the
     range [0, infinity] and -1 to 1 for x in the range [-infinity, infinity].
 
     Useful for cost functions.
     """
     return 2.0 / (1 + exp(-x)) - 1.0
+
 
 def to_equation(coefficients):
     """
@@ -37,10 +40,11 @@ def to_equation(coefficients):
     """
     def f(t):
         total = 0.0
-        for i, c in enumerate(coefficients): 
+        for i, c in enumerate(coefficients):
             total += c * t ** i
         return total
     return f
+
 
 def differentiate(coefficients):
     """
@@ -49,8 +53,9 @@ def differentiate(coefficients):
     """
     new_cos = []
     for deg, prev_co in enumerate(coefficients[1:]):
-        new_cos.append((deg+1) * prev_co)
+        new_cos.append((deg + 1) * prev_co)
     return new_cos
+
 
 def nearest_approach_to_any_vehicle(traj, vehicles):
     """
@@ -58,14 +63,15 @@ def nearest_approach_to_any_vehicle(traj, vehicles):
     """
     closest = 999999
     for v in vehicles.values():
-        d = nearest_approach(traj,v)
+        d = nearest_approach(traj, v)
         if d < closest:
             closest = d
     return closest
 
+
 def nearest_approach(traj, vehicle):
     closest = 999999
-    s_,d_,T = traj
+    s_, d_, T = traj
     s = to_equation(s_)
     d = to_equation(d_)
     for i in range(100):
@@ -73,10 +79,11 @@ def nearest_approach(traj, vehicle):
         cur_s = s(t)
         cur_d = d(t)
         targ_s, _, _, targ_d, _, _ = vehicle.state_in(t)
-        dist = sqrt((cur_s-targ_s)**2 + (cur_d-targ_d)**2)
+        dist = sqrt((cur_s - targ_s) ** 2 + (cur_d - targ_d) ** 2)
         if dist < closest:
             closest = dist
     return closest
+
 
 def show_trajectory(s_coeffs, d_coeffs, T, vehicle=None):
     s = to_equation(s_coeffs)
@@ -87,7 +94,7 @@ def show_trajectory(s_coeffs, d_coeffs, T, vehicle=None):
         X2 = []
         Y2 = []
     t = 0
-    while t <= T+0.01:
+    while t <= T + 0.01:
         X.append(s(t))
         Y.append(d(t))
         if vehicle:
@@ -95,10 +102,11 @@ def show_trajectory(s_coeffs, d_coeffs, T, vehicle=None):
             X2.append(s_)
             Y2.append(d_)
         t += 0.25
-    plt.scatter(X,Y,color="blue")
+    plt.scatter(X, Y, color="blue")
     if vehicle:
-        plt.scatter(X2, Y2,color="red")
+        plt.scatter(X2, Y2, color="red")
     plt.show()
+
 
 def get_f_and_N_derivatives(coeffs, N=3):
     functions = [to_equation(coeffs)]
@@ -106,4 +114,3 @@ def get_f_and_N_derivatives(coeffs, N=3):
         coeffs = differentiate(coeffs)
         functions.append(to_equation(coeffs))
     return functions
-
