d633d51caf69817e5c36d71718e630b0a7c33163
Wubai Zhou <wubai.zhou@uber.com>
2018-05-06 22:43:20 -0700

    add Eigen library

---
+++ Path-Planning-Project/src/cost.cpp
@@ -0,0 +1,8 @@
+//
+//  cost.cpp
+//  path_planning
+//
+//  Created by Wubai Zhou on 5/6/18.
+//
+
+#include <stdio.h>
---
+++ Path-Planning-Project/src/cost.h
@@ -0,0 +1,12 @@
+//
+//  cost.h
+//  Path_Planning
+//
+//  Created by Wubai Zhou on 5/6/18.
+//
+
+#ifndef cost_h
+#define cost_h
+
+
+#endif /* cost_h */
